c331573d86f681f5bb09adf5ac6339a55e931dcf
Paloma Souza <papsouza@br000f5h4ph49fg.ml.com>
2024-12-17 16:11:56 -0300

    start of requirement #5

---
+++ internal/employeers.go
@@ -0,0 +1,16 @@
+package dto
+
+type EmployeeGet struct {
+	Id           int    `json:"id"`
+	CardNumberId string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	WarehouseId  int    `json:"warehouse_id"`
+}
+
+type EmployeePatch struct {
+	CardNumberId *string `json:"card_number_id,omitempty"`
+	FirstName    *string `json:"first_name,omitempty"`
+	LastName     *string `json:"last_name,omitempty"`
+	WarehouseId  *int    `json:"warehouse_id,omitempty"`
+}
