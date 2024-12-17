aef587b13d2e2329ed8633ca511892ba12b9d0ce
PauloCesarRR <pribeirorpontes@gmail.com>
2024-12-17 15:33:34 -0300

    feat: created seller handler and seller dtos

---
+++ internal/dto/seller_dto.go
@@ -0,0 +1,22 @@
+package dto
+
+type SellersGetDto struct {
+	Id          int    `json:"id"`
+	Cid         int    `json:"cid"`
+	CompanyName string `json:"company_name"`
+	Address     string `json:"address"`
+	Telephone   string `json:"telephone"`
+}
+
+type SellersPostDto struct {
+	CID         int    `json:"cid"`
+	CompanyName string `json:"company_name"`
+	Address     string `json:"address"`
+	Telephone   string `json:"telephone"`
+}
+
+type SellersUpdateDto struct {
+	CompanyName *string `json:"company_name"`
+	Address     *string `json:"address"`
+	Telephone   *string `json:"telephone"`
+}
