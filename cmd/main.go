38017719091391ec02cb83f144c055655b81467e
Fabio Nacarelli <fabio.fnacarelli@mercadolivre.com>
2024-12-17 13:18:11 -0300

    get all working fine

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/meli-fresh-products-api-backend-t1/internal/handler"
+	"github.com/meli-fresh-products-api-backend-t1/internal/repository"
+	"github.com/meli-fresh-products-api-backend-t1/internal/service"
 )
 
 func main() {
 	cr := chi.NewRouter()
+	repo := repository.NewBuyerMap()
+	svc := service.NewBuyerService(repo)
+	hd := handler.NewBuyerHandlerDefault(svc)
 
-	cr.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong\n"))
+	cr.Route("/buyer", func(r chi.Router) {
+		r.Get("/", hd.GetAll())
 	})
 	if err := http.ListenAndServe(":8080", cr); err != nil {
 		log.Fatal(err)
