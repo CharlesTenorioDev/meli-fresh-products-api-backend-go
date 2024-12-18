d426f20f1c91e0db65d3859c6f38b03765fbd2f1
AxelGeorg <axel.gmueller@mercadolivre.com>
2024-12-18 09:33:29 -0300

    feature: section endpoints finished

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/meli-fresh-products-api-backend-t1/internal/handler"
+	"github.com/meli-fresh-products-api-backend-t1/internal/repository"
+	"github.com/meli-fresh-products-api-backend-t1/internal/service"
 )
 
 func main() {
-	cr := chi.NewRouter()
+	router := chi.NewRouter()
 
-	cr.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong\n"))
+	rpS := repository.NewRepositorySection()
+	rpW := repository.NewRepositoryWareHouse()
+
+	sv := service.NewServiceSection(rpS, rpW)
+	hd := handler.NewHandlerSection(sv)
+
+	router.Route("/api/v1/sections", func(r chi.Router) {
+		r.Get("/", hd.GetAll)
+		r.Get("/{id}", hd.GetByID)
+		r.Post("/", hd.Create)
+		r.Patch("/{id}", hd.Update)
+		r.Delete("/{id}", hd.Delete)
 	})
-	if err := http.ListenAndServe(":8080", cr); err != nil {
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
 	}
 }
