46f1b0826c1960f4bcec09f0e63df0146df5643e
AxelGeorg <axel.gmueller@mercadolivre.com>
2024-12-19 11:30:36 -0300

    Feat: Add product_type json

---
+++ internal/loader/product_type.go
@@ -0,0 +1,53 @@
+package loader
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+const (
+	localFileProdTypeJson = "db/product_type.json"
+)
+
+type ProductType struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func ReadAllProductsTypeToFile() ([]*ProductType, error) {
+	var productTypeList []*ProductType
+
+	file, err := os.Open(localFileProdTypeJson)
+	if err != nil {
+		if os.IsNotExist(err) {
+			file, err = os.Create(localFileProdTypeJson)
+			if err != nil {
+				return nil, err
+			}
+			defer file.Close()
+
+			initialData := []ProductType{}
+			writer := json.NewEncoder(file)
+			if err := writer.Encode(initialData); err != nil {
+				return nil, err
+			}
+
+			return productTypeList, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := json.NewDecoder(file)
+	err = reader.Decode(&productTypeList)
+	if err != nil {
+		if err == io.EOF {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return productTypeList, nil
+}
