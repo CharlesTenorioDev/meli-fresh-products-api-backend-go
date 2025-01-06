59f82676949e0ceccb09f7090cb15732ee9dc1db
PauloCesarRR <pribeirorpontes@gmail.com>
2025-01-06 11:27:04 -0300

    feat: added validations to locality id

--- internal/locality.go
+++ internal/locality.go
@@ -20,6 +20,19 @@ var (
 )
 
 func (l *Locality) Validate() (causes []Causes) {
+
+	if validator.IntIsNegative(l.ID) {
+		causes = append(causes, Causes{
+			Field:   "locality_id",
+			Message: "Locality ID cannot be negative",
+		})
+	}
+	if validator.IntIsZero(l.ID) {
+		causes = append(causes, Causes{
+			Field:   "locality_id",
+			Message: "Locality ID is required",
+		})
+	}
 	if !validator.String(l.LocalityName, 1, 255) {
 		causes = append(causes, Causes{
 			Field:   "locality_name",
