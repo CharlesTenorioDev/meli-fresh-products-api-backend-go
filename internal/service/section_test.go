4d3ee22fe829160a46422336fd0d3e363d8ba73c
AxelGeorg <axel.gmueller@mercadolivre.com>
2025-01-22 17:03:48 -0300

    Test section service

--- internal/service/section_test.go
+++ internal/service/section_test.go
@@ -53,82 +53,73 @@ func (r *SectionRepositoryMock) Delete(id int) error {
 	return args.Error(0)
 }
 
-func newSectionService() (*service.SectionService, *SectionRepositoryMock) {
+func newSectionService() (*service.SectionService, *SectionRepositoryMock, *service.ProductTypeRepositoryMock, *WarehouseRepositoryMock) {
 	rpSection := new(SectionRepositoryMock)
 	rpProductType := new(service.ProductTypeRepositoryMock)
 	rpProduct := new(repositoryProductMock)
 	rpWareHouse := new(WarehouseRepositoryMock)
 
-	return service.NewServiceSection(rpSection, rpProductType, rpProduct, rpWareHouse), rpSection
+	return service.NewServiceSection(rpSection, rpProductType, rpProduct, rpWareHouse), rpSection, rpProductType, rpWareHouse
 }
 
-func TestCreate_Section(t *testing.T) {
-	sectionCreate := internal.Section{
-		ID:                 0,
-		SectionNumber:      101,
+func newTestSection(id int, sectionNumber int, warehouseID int, productTypeID int) internal.Section {
+	return internal.Section{
+		ID:                 id,
+		SectionNumber:      sectionNumber,
 		CurrentTemperature: 22.5,
 		MinimumTemperature: 15.0,
 		CurrentCapacity:    50,
 		MinimumCapacity:    30,
 		MaximumCapacity:    100,
-		WarehouseID:        4,
-		ProductTypeID:      3,
+		WarehouseID:        warehouseID,
+		ProductTypeID:      productTypeID,
 	}
+}
 
+func TestCreateSection(t *testing.T) {
 	t.Run("successfully create a new section", func(t *testing.T) {
-		sv, rpSection := newSectionService()
+		sv, rpSection, rpProductType, rpWareHouse := newSectionService()
 
-		rpSection.On("FindAll").Return([]internal.Section{}, nil)
-		rpSection.On("Save", &sectionCreate).Return(1, nil)
+		sectionCreate := newTestSection(0, 101, 4, 3)
+
+		rpSection.On("SectionNumberExists", sectionCreate).Return(false, nil)
+		rpWareHouse.On("FindByID", sectionCreate.WarehouseID).Return(internal.Warehouse{ID: sectionCreate.WarehouseID}, nil)
+		rpProductType.On("FindByID", sectionCreate.ProductTypeID).Return(internal.ProductType{ID: sectionCreate.ProductTypeID}, nil)
+		rpSection.On("Save", &sectionCreate).Return(nil)
 
 		err := sv.Save(&sectionCreate)
 
 		require.NoError(t, err)
 		rpSection.AssertExpectations(t)
+		rpProductType.AssertExpectations(t)
+		rpWareHouse.AssertExpectations(t)
 	})
 
-	t.Run("conflict to create a section when number is already in use", func(t *testing.T) {
-		sv, rpSection := newSectionService()
+	t.Run("return conflict error when number is already in use", func(t *testing.T) {
+		sv, rpSection, rpProductType, rpWareHouse := newSectionService()
 
-		rpSection.On("FindAll").Return([]internal.Section{
-			{SectionNumber: 101},
-		}, nil)
+		sectionCreate := newTestSection(0, 101, 4, 3)
+
+		rpSection.On("SectionNumberExists", sectionCreate).Return(true, nil)
 
 		err := sv.Save(&sectionCreate)
 
 		require.ErrorIs(t, err, internal.ErrSectionNumberAlreadyInUse)
 		rpSection.AssertExpectations(t)
+		rpProductType.AssertExpectations(t)
+		rpWareHouse.AssertExpectations(t)
 	})
 }
 
-func TestRead_Section(t *testing.T) {
-	sectionsRead := []internal.Section{
-		{
-			ID:                 0,
-			SectionNumber:      101,
-			CurrentTemperature: 22.5,
-			MinimumTemperature: 15.0,
-			CurrentCapacity:    50,
-			MinimumCapacity:    30,
-			MaximumCapacity:    100,
-			WarehouseID:        2,
-			ProductTypeID:      1,
-		},
-		{
-			ID:                 0,
-			SectionNumber:      102,
-			CurrentTemperature: 29.5,
-			MinimumTemperature: 19.0,
-			CurrentCapacity:    56,
-			MinimumCapacity:    36,
-			MaximumCapacity:    102,
-			WarehouseID:        1,
-			ProductTypeID:      4,
-		},
-	}
-
+func TestReadSection(t *testing.T) {
 	t.Run("successfully read all sections", func(t *testing.T) {
-		sv, rpSection := newSectionService()
+		sv, rpSection, _, _ := newSectionService()
+
+		sectionsRead := []internal.Section{
+			newTestSection(0, 101, 2, 1),
+			newTestSection(0, 102, 1, 4),
+		}
+
 		rpSection.On("FindAll").Return(sectionsRead, nil)
 
 		sections, err := sv.FindAll()
@@ -139,30 +130,22 @@ func TestRead_Section(t *testing.T) {
 	})
 
 	t.Run("return error when reading a nonexistent section by ID", func(t *testing.T) {
-		sv, rpSection := newSectionService()
-		rpSection.On("FindByID", 1).Return(internal.Section{}, internal.ErrSectionNotFound)
+		sv, rpSection, _, _ := newSectionService()
+		expectedError := internal.ErrSectionNotFound
+
+		rpSection.On("FindByID", 1).Return(internal.Section{}, expectedError)
 
 		_, err := sv.FindByID(1)
 
 		require.Error(t, err)
-		require.EqualError(t, err, "section not found")
+		require.ErrorIs(t, err, expectedError)
 		rpSection.AssertExpectations(t)
 	})
 
 	t.Run("successfully read an existing section by ID", func(t *testing.T) {
-		expectedSection := internal.Section{
-			ID:                 0,
-			SectionNumber:      101,
-			CurrentTemperature: 22.5,
-			MinimumTemperature: 15.0,
-			CurrentCapacity:    50,
-			MinimumCapacity:    30,
-			MaximumCapacity:    100,
-			WarehouseID:        4,
-			ProductTypeID:      3,
-		}
+		sv, rpSection, _, _ := newSectionService()
+		expectedSection := newTestSection(2, 101, 4, 3)
 
-		sv, rpSection := newSectionService()
 		rpSection.On("FindByID", 2).Return(expectedSection, nil)
 
 		section, err := sv.FindByID(2)
@@ -173,17 +156,15 @@ func TestRead_Section(t *testing.T) {
 	})
 }
 
-func TestUpdate_Section(t *testing.T) {
+func TestUpdateSection(t *testing.T) {
 	t.Run("return error when updating a nonexistent section", func(t *testing.T) {
-		sv, rpSection := newSectionService()
+		sv, rpSection, _, _ := newSectionService()
 
 		updates := map[string]interface{}{
-			"maximum_capacity": 150,
-			"warehouse_id":     5,
-			"product_type_id":  4,
+			"maximum_capacity": 150.0,
 		}
 
-		rpSection.On("Update", 1, updates).Return(internal.Section{}, internal.ErrSectionNotFound)
+		rpSection.On("FindByID", 1).Return(internal.Section{}, internal.ErrSectionNotFound)
 
 		updatedSection, err := sv.Update(1, updates)
 
@@ -194,32 +175,32 @@ func TestUpdate_Section(t *testing.T) {
 	})
 
 	t.Run("successfully update an existing section", func(t *testing.T) {
-		sv, rpSection := newSectionService()
+		sv, rpSection, rpProductType, rpWareHouse := newSectionService()
 
-		existingSection := internal.Section{
-			ID:              1,
-			MaximumCapacity: 100,
-			WarehouseID:     14,
-			ProductTypeID:   2,
-		}
+		existingSection := newTestSection(1, 100, 4, 3)
 
 		updates := map[string]interface{}{
-			"maximum_capacity": 150,
+			"maximum_capacity": 150.0,
 		}
 
-		rpSection.On("Update", 1, updates).Return(existingSection, nil)
+		rpSection.On("FindByID", 1).Return(existingSection, nil)
+		rpWareHouse.On("FindByID", existingSection.WarehouseID).Return(internal.Warehouse{ID: existingSection.WarehouseID}, nil)
+		rpProductType.On("FindByID", existingSection.ProductTypeID).Return(internal.ProductType{ID: existingSection.ProductTypeID}, nil)
+
+		rpSection.On("Update", mock.AnythingOfType("*internal.Section")).Return(nil)
 
 		updatedSection, err := sv.Update(1, updates)
 
 		require.NoError(t, err)
-		require.Equal(t, existingSection, updatedSection)
+		require.NotEqual(t, existingSection, updatedSection)
 		rpSection.AssertExpectations(t)
 	})
 }
 
-func TestDelete_Section(t *testing.T) {
+func TestDeleteSection(t *testing.T) {
 	t.Run("return error when attempting to delete a nonexistent section", func(t *testing.T) {
-		sv, rpSection := newSectionService()
+		sv, rpSection, _, _ := newSectionService()
+
 		rpSection.On("FindByID", 1).Return(internal.Section{}, internal.ErrSectionNotFound)
 
 		err := sv.Delete(1)
@@ -227,9 +208,13 @@ func TestDelete_Section(t *testing.T) {
 		require.ErrorIs(t, err, internal.ErrSectionNotFound)
 		rpSection.AssertExpectations(t)
 	})
+
 	t.Run("successfully delete an existing section", func(t *testing.T) {
-		sv, rpSection := newSectionService()
-		rpSection.On("FindByID", 1).Return(internal.Section{ID: 1}, nil)
+		sv, rpSection, _, _ := newSectionService()
+
+		existingSection := newTestSection(1, 101, 4, 3)
+
+		rpSection.On("FindByID", 1).Return(existingSection, nil)
 		rpSection.On("Delete", 1).Return(nil)
 
 		err := sv.Delete(1)
