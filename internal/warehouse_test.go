9ec77a45e6ac7bead6dcc8498ee22e7814ee747b
PauloCesarRR <pribeirorpontes@gmail.com>
2025-02-07 10:18:46 -0300

    test: finalized cases of warehouse test

--- internal/warehouse_test.go
+++ internal/warehouse_test.go
@@ -6,37 +6,224 @@ import (
 	"testing"
 )
 
-func TestWarehouse_Validate(t *testing.T) {
+func TestWarehouseValidate(t *testing.T) {
 	tests := []struct {
-		name           string
-		setup          func(t *testing.T) *internal.Warehouse
-		expectedOutput []internal.Causes
+		name      string
+		warehouse internal.Warehouse
+		wantErr   bool
+		causes    []internal.Causes
 	}{
 		{
-			name: "",
-			setup: func(t *testing.T) *internal.Warehouse {
-				return &internal.Warehouse{
-					ID:                 0,
-					WarehouseCode:      "",
-					Address:            "",
-					Telephone:          "",
-					MinimumCapacity:    0,
-					MinimumTemperature: 0,
-				}
+			name: "valid warehouse",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
 			},
-			expectedOutput: []internal.Causes{
-				{},
+			wantErr: false,
+			causes:  nil,
+		},
+		{
+			name: "invalid warehouse - missing warehouse code",
+			warehouse: internal.Warehouse{
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "warehouse_code",
+					Message: "warehouse code is required",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - warehouse code out of range",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "ThisIsAVeryLongWarehouseCodeThatExceedsTheLimitXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "warehouse_code",
+					Message: "warehouse code is out of range",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - missing address",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "address",
+					Message: "address is required",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - address out of range",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "ThisIsAVeryLongAddressThatExceedsTheLimitXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "address",
+					Message: "address is out of range",
+				},
+			},
+		},
+
+		{
+			name: "invalid warehouse - missing telephone",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "telephone",
+					Message: "telephone is required",
+				},
+				{
+					Field:   "telephone",
+					Message: "telephone number is invalid, should be formatted as XX XXXXX-XXXX",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - invalid telephone",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12345678901", // Invalid format
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "telephone",
+					Message: `telephone number is invalid, should be formatted as XX XXXXX-XXXX`,
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - telephone out of range",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12 34567-890111", // Invalid format
+				MinimumCapacity:    100,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "telephone",
+					Message: "telephone number is invalid, should be formatted as XX XXXXX-XXXX",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - zero minimum capacity",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    0,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "minimum_capacity",
+					Message: "minimum capacity is required",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - negative minimum capacity",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    -10,
+				MinimumTemperature: 20.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "minimum_capacity",
+					Message: "minimum capacity cannot be negative",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - minimum temperature out of range (too low)",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: -300.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "minimum_temperature",
+					Message: "minimum temperature is out of range",
+				},
+			},
+		},
+		{
+			name: "invalid warehouse - minimum temperature out of range (too high)",
+			warehouse: internal.Warehouse{
+				WarehouseCode:      "WH-123",
+				Address:            "Test Address",
+				Telephone:          "12 34567-8901",
+				MinimumCapacity:    100,
+				MinimumTemperature: 1200.0,
+			},
+			wantErr: true,
+			causes: []internal.Causes{
+				{
+					Field:   "minimum_temperature",
+					Message: "minimum temperature is out of range",
+				},
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ware := tt.setup(t)
-
-			actualOutput := ware.Validate()
-
-			assert.Equal(t, tt.expectedOutput, actualOutput)
+			causes := tt.warehouse.Validate()
+			if tt.wantErr {
+				assert.Equal(t, tt.causes, causes)
+			} else {
+				assert.Nil(t, causes)
+			}
 		})
 	}
 }
